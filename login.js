f7e104c3c351a823ad6ce297856dc172dd711ae7
mohorgoswami <prajnagoswami22@gmail.com>
2023-08-28 11:16:40 +0530

    add

---
+++ login.js
@@ -0,0 +1,25 @@
+async function login(e){
+    try{
+        e.preventDefault();
+        console.log(e.target.email.value);
+
+        const signupdetails={
+            username:e.target.username.value,
+            email:e.target.email.value,
+            password:e.target.password.value
+        
+        }
+        console.log(signupdetails);
+        const response=await axios.post('http://localhost:3000/user/signup',signupdetails);
+        if(response.status===201){
+            window.location.href="../Login/login.html"
+        }
+        else{
+            throw new Error('Failed to login')
+        }
+    }
+    catch(err){
+        document.body.innerHTML+=`<div style="color:red;">${err}</div>`;
+
+    }
+}
